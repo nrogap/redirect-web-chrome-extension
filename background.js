555efc38775f5fee5402a1c98dd7bb2c9172104c
Pagorn Phusaisakul <nrogap.p@gmail.com>
2022-07-28 09:16:26 +0700

    Can send multiple source URLs

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-const SOURCE_URL = 'msn.com'
+const SOURCE_URLS = ['go.microsoft.com/fwlink', 'msn.com']
 const DESTINATION_URL = 'https://youtube.com'
 const ERROR_CANNOT_EDITED = 'Error: Tabs cannot be edited right now (user may be dragging a tab).'
 
@@ -8,15 +8,19 @@ chrome.tabs.onUpdated.addListener(redirectMSNOnUpdated);
 chrome.tabs.onRemoved.removeListener(redirectMSNOnCreated);
 chrome.tabs.onRemoved.removeListener(redirectMSNOnUpdated);
 
-async function redirectTo(tab, sourceUrl, destinationUrl) {
-  if (tab.url?.includes(sourceUrl)) {
-    await chrome.tabs.update(tab.id, { url: destinationUrl });
+async function redirectTo(tab, sourceUrls, destinationUrl) {
+  length = sourceUrls.length
+
+  for(let i = 0; i < length; i++){
+    if (tab.url?.includes(sourceUrls[i])) {
+      await chrome.tabs.update(tab.id, { url: destinationUrl });
+    }
   }
 }
 
 async function redirectMSNOnCreated(tab) {
   try {
-    await redirectTo(tab, SOURCE_URL, DESTINATION_URL)
+    await redirectTo(tab, SOURCE_URLS, DESTINATION_URL)
   } catch (error) {
     if (error == ERROR_CANNOT_EDITED) {
       setTimeout(() => redirectMSNOnCreated(tab), 50);
@@ -28,7 +32,7 @@ async function redirectMSNOnCreated(tab) {
 
 async function redirectMSNOnUpdated(tabId, changeInfo, tab) {
   try {
-    await redirectTo(tab, SOURCE_URL, DESTINATION_URL)
+    await redirectTo(tab, SOURCE_URLS, DESTINATION_URL)
   } catch (error) {
     if (error == ERROR_CANNOT_EDITED) {
       setTimeout(() => redirectMSNOnUpdated(tabId, changeInfo, tab), 50);
