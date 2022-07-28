a56288fe1593a9a6be3244308a4ca4a96410c8ab
Pagorn Phusaisakul <nrogap.p@gmail.com>
2022-07-28 10:55:32 +0700

    Rename function

--- background.js
+++ background.js
@@ -2,11 +2,11 @@ const SOURCE_URLS = ['go.microsoft.com/fwlink', 'msn.com']
 const DESTINATION_URL = 'https://youtube.com'
 const ERROR_CANNOT_EDITED = 'Error: Tabs cannot be edited right now (user may be dragging a tab).'
 
-chrome.tabs.onCreated.addListener(redirectMSNOnCreated);
-chrome.tabs.onUpdated.addListener(redirectMSNOnUpdated);
+chrome.tabs.onCreated.addListener(tabOnCreated);
+chrome.tabs.onUpdated.addListener(tabOnUpdated);
 
-chrome.tabs.onRemoved.removeListener(redirectMSNOnCreated);
-chrome.tabs.onRemoved.removeListener(redirectMSNOnUpdated);
+chrome.tabs.onRemoved.removeListener(tabOnCreated);
+chrome.tabs.onRemoved.removeListener(tabOnUpdated);
 
 async function redirectTo(tab, sourceUrls, destinationUrl) {
   length = sourceUrls.length
@@ -18,24 +18,24 @@ async function redirectTo(tab, sourceUrls, destinationUrl) {
   }
 }
 
-async function redirectMSNOnCreated(tab) {
+async function tabOnCreated(tab) {
   try {
     await redirectTo(tab, SOURCE_URLS, DESTINATION_URL)
   } catch (error) {
     if (error == ERROR_CANNOT_EDITED) {
-      setTimeout(() => redirectMSNOnCreated(tab), 50);
+      setTimeout(() => tabOnCreated(tab), 50);
     } else {
       console.error(error);
     }
   }
 }
 
-async function redirectMSNOnUpdated(tabId, changeInfo, tab) {
+async function tabOnUpdated(tabId, changeInfo, tab) {
   try {
     await redirectTo(tab, SOURCE_URLS, DESTINATION_URL)
   } catch (error) {
     if (error == ERROR_CANNOT_EDITED) {
-      setTimeout(() => redirectMSNOnUpdated(tabId, changeInfo, tab), 50);
+      setTimeout(() => tabOnUpdated(tabId, changeInfo, tab), 50);
     } else {
       console.error(error);
     }
